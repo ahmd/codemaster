2f005f4516f88a8f9f059067309bfb1edaf34c62
Ahmd Rmdan <ahmd.rmdn@gmail.com>
2017-11-16 20:16:10 +0200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    particlesJS.load('particles-js', 'assets/particles/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+      
+});
